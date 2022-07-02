381cfa1e52c3aa79013e2a0d1634c3d933fbb127
fatihguzel <20060393@stu.omu.edu.tr>
2022-07-03 00:43:06 +0300

    update page

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/sign_up",(req,res)=>{
         console.log("Record Inserted Successfully");
     });
 
-    return res.redirect('signup_success.html')
+    return res.redirect('index.html')
 
 })
 
